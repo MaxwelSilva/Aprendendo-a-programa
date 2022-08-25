3e79552094a5108796a1b177de579bb9019ccfaa
Maxwel Silva <maxwelsilva.ms20@gmail.com>
2022-08-25 01:11:03 -0300

    Games

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,10 @@
+let nome = "Maxwel"
+console.log (nome)
+let email = "Maxwelsilva.ms20@gmail.com"
+console.log(email)
+let profissao = "Futuro progamdor de games"
+console.log(profissao)
+let idade = "21"
+console.log(idade)
+let lugar = "alemanha" 
+console.log(lugar)
